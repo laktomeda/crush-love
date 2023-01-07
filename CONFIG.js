3a356c71f597c26b79df7fa382ff17e417715499
laktomeda <119561608+laktomeda@users.noreply.github.com>
2023-01-07 04:02:30 -0600

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Te quiero mucho Maris, este regalo es para tu <3",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+    "La neta perdon por no dartelo antes, pero espero q te guste aunque fucione mas o menos unu ^^",
+  buttonYes: "LO ACEPTO<3",
+  buttonNo: "No autorizo",
+  titleModar: "Espero que te guste tu propio asistente jjj",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'La neta copia el link pq ya no supe como hacer para que te direccionara al gugul drive jajsjaks </br> (kinl) "',
 };
