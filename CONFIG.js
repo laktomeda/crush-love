e867e26809c70e880909131ba98502f9e20967ff
Emi <119561608+laktomeda@users.noreply.github.com>
2024-11-27 17:44:43 -0600

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Te quiero mucho Maris, este regalo es para tu <3",
+  titleHeader: "TE AMOO MUCHO MI AMOOOR, este regalo es para ti <3",
   descriptionHeader:
-    "La neta perdon por no dartelo antes, pero espero q te guste aunque fucione mas o menos unu ^^",
+    "Felices un año y tres meses mi amoor",
   buttonYes: "LO ACEPTO<3",
   buttonNo: "No autorizo",
-  titleModar: "Espero que te guste tu propio asistente jjj",
+  titleModar: "Sin duda, de nalguitas de bombon para chocolatito caliente",
   descriptionModar:
-    'La neta copia el link pq ya no supe como hacer para que te direccionara al gugul drive jajsjaks </br> (https://drive.google.com/file/d/1D3Ln-l0j19Rftm-qG-wB_T6-M0jND61Q/view?usp=sharing) "',
+    'Felices 1 año y 3 meses, mi amor. Han pasado 455 días desde que me abrí a ti y te hice la pregunta que transformaría mi vida para siempre. Una pregunta que cambió mi forma de pensar, de actuar y de reaccionar. Palabras que, por sí solas, podrían parecer vacías, pero que nosotros llenamos de significado, creando el sentido más maravilloso y bello del mundo. Vibraciones vocales que alteraron nuestro destino, pronunciadas con valentía y sin temor. Sin duda, Dios es amor. Desde el primer momento en que vi tu mirada, sentí ese amor: un cálido abrazo, una grandeza inmensa, una montaña por escalar llena de misterios, cuevas y manantiales. Algo tan poderoso que transforma al más cobarde en un héroe valiente. El amor nos impulsa a ser mejores personas. Sin lugar a dudas, eso fue lo que experimenté cuando te besé por primera vez. Un beso tan especial y mágico, una experiencia única. Ese día fue como un Big Bang; de dos extraños que no se habían visto en mucho tiempo surgió un nuevo universo. Un espacio donde podemos compartir pensamientos, opiniones y pasados; un lugar de libertad y seguridad. A pesar de las supernovas que puedan surgir en nuestro camino, siempre encontraremos la paz. Por eso tiene tanto significado la frase "Dios es amor": de la nada pueden nacer universos donde coexistimos en armonía. Regi, gracias por seguir construyendo este universo mutuo conmigo. Eres una experiencia inigualable, y sin duda volvería a elegirte por toda la eternidad. Seguiría enamorándome de ti una y otra vez. Te amo muchísimo. </br> (https://drive.google.com/file/d/1D3Ln-l0j19Rftm-qG-wB_T6-M0jND61Q/view?usp=sharing) "',
 };
